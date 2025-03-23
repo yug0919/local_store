b2bd19f3d4309089eb7d0b8688df9763bc5a52ec
yug0919 <yugal.bhendarkar13@gmail.com>
2025-03-24 02:35:04 +0530

    upadeted login form

--- app/controllers/customers/registrations_controller.rb
+++ app/controllers/customers/registrations_controller.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
 class Customers::RegistrationsController < Devise::RegistrationsController
+before_action :configure_sign_up_params, only: [:create]
+before_action :configure_account_update_params, only: [:update]
+
+#additional parameters for sign up
+
+def configure_sign_up_params
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:phone,:address])
+end
+
+def configure_account_update_params
+  devise_parameter_sanitizer.permit(:account_update, keys: [:name,:phone,:address])
+end
+
+
+
 
 
 

3d9c95ac85ee968583af972056444dbdc334462a
yug0919 <yugal.bhendarkar13@gmail.com>
2025-03-24 03:25:47 +0530

    updated routes of logout button

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -4,7 +4,7 @@ class Customers::SessionsController < Devise::SessionsController
 
 
   def after_sign_out_path_for(_resource_or_scope)
-    new_customer_session_path
+    root_path
   end
 
   def after_sign_in_path_for(resource_or_scope)
